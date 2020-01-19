82b1a891aa29c110c0f528801a4d70a1f152adae
vique254 <victoronyango051@gmail.com>
2020-01-19 15:26:48 +0300

    change database content

--- hood/settings.py
+++ hood/settings.py
@@ -74,11 +74,12 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'moringa',
+    'PASSWORD':'vique',
     }
 }
 
