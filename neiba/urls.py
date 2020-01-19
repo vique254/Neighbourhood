b8f06091c5ea7087db46b2ddd839cd7f902e330c
vique254 <victoronyango051@gmail.com>
2020-01-20 00:50:46 +0300

    create urls for profile and business

--- neiba/urls.py
+++ neiba/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns =[
     url('^$',views.home,name= 'home '),
     url(r'^$profile/',views.profile,name='profile'),
+    url(r'profile/edit$', views.edit_profile, name='edit'),
+    url(r'business$', views.business, name='business'),
+    url(r'business/new$', views.new_biz, name='new_biz'),
 ]
