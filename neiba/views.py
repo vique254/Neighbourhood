f2808b551873016f68cd5edf33a9c2b915dcd00a
vique254 <victoronyango051@gmail.com>
2020-01-19 23:44:21 +0300

    create profile view function

--- neiba/views.py
+++ neiba/views.py
@@ -1,7 +1,22 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
 def home(request):
+      try: 
+        current_user = request.user
+        profile = Profile.objects.get(user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('edit')
     return render (request,'home.html')
+
+
+
+
+def profile(request):
+    current_user =request.user
+    profile=Profile.objects.get(user=current_user)
+    
+    return render(request,'profile.html',{'profile':profile})
