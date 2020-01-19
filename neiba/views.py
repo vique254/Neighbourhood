f85938ded9f61a18bb3e48898f1a8b896233bb27
vique254 <victoronyango051@gmail.com>
2020-01-20 00:31:05 +0300

    add view functions

--- neiba/views.py
+++ neiba/views.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -21,3 +23,34 @@ def profile(request):
     profile=Profile.objects.get(user=current_user)
     
     return render(request,'profile.html',{'profile':profile})
+
+
+def edit_profile(request):
+    current_user=request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+    else:
+        form = ProfileForm()
+    return render(request,'edit_profile.html',{"form":form})
+  
+  @login_required(login_url='/accounts/login/')
+def biz(request):
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user)
+    if request.method == 'POST':
+        form = BusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            biz = form.save(commit = False)
+            biz.user = current_user
+            biz.neighbourhood = profile.neighbourhood
+            biz.save()
+        return redirect('business')
+    else:
+        form = BusinessForm()
+        
+    return render(request,'biz.html',{'form':form}) 
